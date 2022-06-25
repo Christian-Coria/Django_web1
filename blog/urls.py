02ae1d834550a490779bb201ccf3e863cfeab043
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-06-25 20:19:00 -0300

    creacion de formularios

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
+from venv import create
 from django.urls import path
-from .views import BlogListView
+from .views import BlogCreateViews, BlogListView
 
 app_name = "blog"
 
 urlpatterns = [
-    path('',BlogListView.as_view(), name = "home")
+    path('',BlogListView.as_view(), name = "home"),
+    path('create/',BlogCreateViews.as_view(), name="create"),
 ]
  
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,39 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 
+from blog.models import Post
+from .forms import PostCreateForms
 # Create your views here.
 class BlogListView(View):
     def get(self,request,*args,**kwargs):
-        context = {}
+        context = {   
+
+        }
         return render(request,'blog_list.html', context)
+
+class BlogCreateViews(View):
+    def get(self,request,*args,**kwargs):
+        form=PostCreateForms()
+        context = {   
+              'form':form
+        }
+        return render(request,'blog_create.html', context)
+
+    def post(self,request,*args,**kwargs):
+        if request.method=="POST":
+            form = PostCreateForms(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content= form.cleaned_data.get('content')
+
+                p, created = Post.objects.get_or_create(title= title ,content=content )
+                p.save()
+                return redirect('blog:home')
+
+        context = {   
+
+
+        }
+        return render(request,'blog_create.html', context)
         
